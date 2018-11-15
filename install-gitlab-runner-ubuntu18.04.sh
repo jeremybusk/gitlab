68409679cbab15ecf2e3c3ac6fec677383abd84c
Jeremy Busk <jeremybusk@gmail.com>
2018-11-15 14:57:42 -0700

    Update install-gitlab-runner-ubuntu18.04.sh

--- install-gitlab-runner-ubuntu18.04.sh
+++ install-gitlab-runner-ubuntu18.04.sh
@@ -1,2 +1,22 @@
+#!/usr/bin/env bash
+# # For Debian/Ubuntu/Mint Gitlab Runner Install & Register
+# ref https://docs.gitlab.com/runner/install/linux-repository.html
 curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
-apt install gitlab-runner
+
+#
+cat > /etc/apt/preferences.d/pin-gitlab-runner.pref <<EOF
+ Explanation: Prefer GitLab provided packages over the Debian native ones
+ Package: gitlab-runner
+ Pin: origin packages.gitlab.com
+ Pin-Priority: 1001
+ EOF
+ 
+sudo apt-get update
+sudo apt-get install gitlab-runner
+ 
+## Update 
+# sudo apt-get update
+# sudo apt-get install gitlab-runner
+
+# Register runner
+sudo gitlab-runner register
