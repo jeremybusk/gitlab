e9d9947be0b63a7671c90ed1122c18292f9b48c4
Jeremy Busk <jeremybusk@gmail.com>
2018-11-15 15:54:40 -0700

    Update install-gitlab-runner-ubuntu18.04.sh

--- install-gitlab-runner-ubuntu18.04.sh
+++ install-gitlab-runner-ubuntu18.04.sh
@@ -1,22 +1,42 @@
 #!/usr/bin/env bash
-# # For Debian/Ubuntu/Mint Gitlab Runner Install & Register
+## Debian/Ubuntu/Mint Install Docker CE, Gitlab Runner & Register
 # ref https://docs.gitlab.com/runner/install/linux-repository.html
-curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
 
-#
+# # Install Docker-CE
+# # https://docs.docker.com/install/linux/docker-ce/ubuntu/
+# sudo apt-get update
+# sudo apt-get -y install \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     software-properties-common
+# 
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
+# 
+# sudo apt-get update
+# sudo apt-get -y install docker-ce
+# 
+# 
+# 
+# curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
+
 cat > /etc/apt/preferences.d/pin-gitlab-runner.pref <<EOF
  Explanation: Prefer GitLab provided packages over the Debian native ones
  Package: gitlab-runner
  Pin: origin packages.gitlab.com
  Pin-Priority: 1001
- EOF
+EOF
  
 sudo apt-get update
-sudo apt-get install gitlab-runner
+sudo apt-get -y install gitlab-runner
  
 ## Update 
 # sudo apt-get update
-# sudo apt-get install gitlab-runner
-
+# sudo apt-get -y install gitlab-runner
 # Register runner
 sudo gitlab-runner register
+
